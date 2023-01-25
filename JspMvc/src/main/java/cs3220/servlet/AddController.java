39889f83dff6d72147ef2cc890a835c4cb72a7d7
hgomez32 <102192845+hgomez32@users.noreply.github.com>
2023-01-25 15:01:22 -0800

    Add files via upload

---
+++ JspMvc/src/main/java/cs3220/servlet/AddController.java
@@ -0,0 +1,43 @@
+package cs3220.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddController
+ */
+@WebServlet("/AddController")
+public class AddController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/AddForm.jsp").forward(request, response);
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int a = Integer.parseInt(request.getParameter("a"));
+		int b = Integer.parseInt(request.getParameter("b"));
+		int sum = a + b;
+		request.setAttribute("a", a);
+		request.setAttribute("b", b);
+		request.setAttribute("sum", sum);
+		request.getRequestDispatcher("/AddResult.jsp").forward(request, response);
+		
+	}
+
+}
---
+++ JspMvc/src/main/webapp/AddForm.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="AddController" method="post">
+a:<input type="text" name ="a"><br>
+b:<input type="text" name="b"><br>
+<button>Add</button>
+</form>
+
+</body>
+</html>
